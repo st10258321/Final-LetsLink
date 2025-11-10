8bc802775361da012e37b7f6dbb4545448e2bd57
st10258321 <ST10258321@vcconnect.edu.za>
2025-11-10 14:16:01 +0200

    added file

---
+++ app/src/main/java/com/example/letslink/online_database/SyncDataManager.kt
@@ -0,0 +1,11 @@
+package com.example.letslink.online_database
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+
+class SyncDataManager {
+    private val database = FirebaseDatabase.getInstance().reference
+    private val auth = FirebaseAuth.getInstance()
+
+
+}
