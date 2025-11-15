3bd6c8dd72a8ff871a9110a4a42dfc26f7e71d41
ST10268411 <126479662+ST10268411@users.noreply.github.com>
2025-11-15 14:05:09 +0200

    Add files via upload

--- app/src/main/java/com/example/letslink/model/Group.kt
+++ app/src/main/java/com/example/letslink/model/Group.kt
@@ -4,6 +4,7 @@ import android.R
 import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
+import java.lang.reflect.Member
 import java.util.UUID
 
 @Entity(
@@ -28,8 +29,18 @@ data class Group(
 
     val description: String,
 
-    var inviteLink: String? = null
+    var inviteLink: String? = null,
+
 )
 {
     constructor() : this("","", "", "", null)
 }
+data class GroupList(
+    var groupId: String = "",
+    val userId: String = "",
+    val groupName: String = "",
+    val description: String = "",
+    var inviteLink: String? = "",
+    val members: List<String> = emptyList() // Changed from 'member' to 'members'
+)
+
