7cb17976456de3ce13422351762f4738d16255ea
st10258321 <131756687+st10258321@users.noreply.github.com>
2025-11-16 18:47:54 +0200

    Implement SOS feature with location access
    Added location permissions and SOS functionality to send emergency messages with user location.

--- app/src/main/java/com/example/letslink/activities/HomePage.kt
+++ app/src/main/java/com/example/letslink/activities/HomePage.kt
@@ -1,8 +1,15 @@
 package com.example.letslink.fragments
 
 import android.content.Context
+import android.content.Intent
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.net.Uri
 import android.graphics.Rect
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -24,6 +31,7 @@ import com.example.letslink.adapter.TicketMasterAdapter
 import com.example.letslink.local_database.EventDao
 import com.example.letslink.local_database.GroupDao
 import com.example.letslink.local_database.LetsLinkDB
+import com.example.letslink.local_database.UserDao
 import com.example.letslink.model.Event
 import com.example.letslink.model.Group
 import com.example.letslink.model.TMEvent
@@ -32,6 +40,13 @@ import com.example.letslink.online_database.SyncDataManager
 import com.example.letslink.online_database.fb_EventsRepo
 import java.util.UUID
 import com.example.letslink.utils.TranslationManager
+import android.view.MotionEvent
+import androidx.annotation.RequiresPermission
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.letslink.online_database.fb_userRepo
 
 class HomeFragment : Fragment() {
 
@@ -44,6 +59,8 @@ class HomeFragment : Fragment() {
     private lateinit var userId : String
     private lateinit var eventsRepo : fb_EventsRepo
     private lateinit var eventDao : EventDao
+    private lateinit var userDao : UserDao
+    private lateinit var userRepo : fb_userRepo
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -62,6 +79,8 @@ class HomeFragment : Fragment() {
         groupDao = LetsLinkDB.getDatabase(requireContext()).groupDao()
         eventDao = LetsLinkDB.getDatabase(requireContext()).eventDao()
         eventsRepo = fb_EventsRepo(requireContext())
+        userDao = LetsLinkDB.getDatabase(requireContext()).userDao()
+        userRepo = fb_userRepo()
 
         val sharedPref = requireActivity().getSharedPreferences("UserSession", Context.MODE_PRIVATE)
         userId = sharedPref.getString(SessionManager.KEY_USER_ID, null).toString()
@@ -139,6 +158,99 @@ class HomeFragment : Fragment() {
                 .addToBackStack(null)
                 .commit()
         }
+       
+        var phoneNumber = ""
+        var message = ""
+        var userLocation = ""
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(requireActivity(),
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                1001)
+        } else {
+            getCurrentLocation(requireContext()) { location ->
+                if (location != null) {
+                    userLocation = location
+                } else {
+                    userLocation = "No location found"
+                }
+            }
+        }
+
+
+        lifecycleScope.launch {
+            val user = userDao.getUserById(userId!!)
+            if(user != null){
+
+                Log.d("check-user","${user.firstName} = ${user.userId} = ${user.emergencyContact}")
+                message = "LetsLink - SOS Message 🆘🆘🆘 \n ${user.firstName} needs your help, please make your way to them by using their location:\n ${userLocation} "
+                phoneNumber = user.emergencyContact!!
+                if(phoneNumber.isEmpty()){
+                    //check online for emergency contact
+                    userRepo.getUserEmergencyContact(userId!!){ emergencyContact ->
+                        if(emergencyContact.isNotEmpty()){
+                            phoneNumber = emergencyContact
+                        }else{
+                            Toast.makeText(requireContext(),"No emergency contact set",Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            }
+        }
+
+
+        val sosBtn = view.findViewById<CardView>(R.id.sos_layout_btn)
+
+        val handler = Handler(Looper.getMainLooper())
+        var isHolding = false
+        val holdRunnable = Runnable {
+            Log.d("SOS", "Runnabke Fired")
+            isHolding = true
+
+            if (phoneNumber.isNotEmpty()) {
+                val url =
+                    "https://api.whatsapp.com/send?phone=$phoneNumber&text=${Uri.encode(message)}"
+                val intent = Intent(Intent.ACTION_VIEW).apply {
+                    data = Uri.parse(url)
+                    setPackage("com.whatsapp")
+                }
+
+                try {
+                    Log.d("check-intent", "intent is being started")
+                    startActivity(intent)
+                } catch (e: Exception) {
+                    Log.d("check-error", e.toString())
+                    Toast.makeText(requireContext(), "Error sending message", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            } else {
+                Toast.makeText(requireContext(), "No emergency contact set", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+
+        sosBtn.setOnTouchListener{ _, event ->
+            Log.d("SOS-Touch","ACTION ${event.action}")
+            //get users emergency contact
+            when(event.action){
+                MotionEvent.ACTION_DOWN -> {
+                    Log.d("SOS","DOWN")
+                    isHolding = false
+                    handler.postDelayed(holdRunnable, 3000L)
+                }
+                MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                    Log.d("SOS,","UP or cancel")
+                    // Handle touch up event
+                    handler.removeCallbacks(holdRunnable)
+                    if(!isHolding){
+                        Log.d("HOLD","Relwased before 3 seconds")
+
+                    }
+                }
+            }
+            true
+        } 
     }
 
     private fun setupOverlappingCarousel() {
@@ -248,6 +360,45 @@ class HomeFragment : Fragment() {
         builder.setNegativeButton("Cancel", null)
         builder.show()
     }
+       @RequiresPermission(allOf = [Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION])
+    private fun getCurrentLocation(context: Context, callback: (String?) -> Unit) {
+        val locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+
+        val lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+            ?: locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+
+        if (lastLocation != null) {
+            val latitude = lastLocation.latitude
+            val longitude = lastLocation.longitude
+            val mapsUrl = "https://www.google.com/maps/search/?api=1&query=$latitude,$longitude"
+            callback(mapsUrl)
+        } else {
+            // If last location is null, request a single update
+            val listener = object : LocationListener {
+                override fun onLocationChanged(location: Location) {
+                    val latitude = location.latitude
+                    val longitude = location.longitude
+                    val mapsUrl = "https://www.google.com/maps/search/?api=1&query=$latitude,$longitude"
+                    callback(mapsUrl)
+                    locationManager.removeUpdates(this)
+                }
+
+                override fun onProviderDisabled(provider: String) {}
+                override fun onProviderEnabled(provider: String) {}
+                override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
+            }
+
+            try {
+                locationManager.requestSingleUpdate(LocationManager.GPS_PROVIDER, listener, Looper.getMainLooper())
+            } catch (e: SecurityException) {
+                e.printStackTrace()
+                callback(null)
+            }
+        }
+    }
+
+    
 
     override fun onDestroyView() {
         super.onDestroyView()
